70c20951a95850e21cefcdf23fa5344f7a90fbfe
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-06-15 11:57:35 +0900

    fix: ダミーデータ

--- src/testData/articles/index.ts
+++ src/testData/articles/index.ts
@@ -2,7 +2,7 @@ import { Article } from "@/types/category";
 
 export const articlesData: Article[] = [
   { id: 0, title: "テストタイトル1", content: "...", category: "technology" },
-  { id: 2, title: "テストタイトル2", content: "...", category: "health" },
-  { id: 3, title: "テストタイトル3", content: "...", category: "finance" },
-  { id: 4, title: "テストタイトル4", content: "...", category: "lifestyle" },
+  { id: 1, title: "テストタイトル2", content: "...", category: "health" },
+  { id: 2, title: "テストタイトル3", content: "...", category: "finance" },
+  { id: 3, title: "テストタイトル4", content: "...", category: "lifestyle" },
 ];
